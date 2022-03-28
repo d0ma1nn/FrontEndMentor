5fb99c0e1f90a86a807c4c9bee344170065b5536
d0ma1nn <dominikculjak@yahoo.com>
2022-03-28 12:31:46 +0200

    repopo

--- script.js
+++ script.js
@@ -19,15 +19,21 @@ window.addEventListener("scroll", function () {
   let offset = window.pageYOffset;
   console.log(offset);
   const phone = document.getElementById("absolute");
-
+  const pos = phone.style.display;
   // phone.style.transform = translate(40 + "rem" * offset, offset * 100 + "px");
   // phone.style.top = offset * 0.2 + "px";
 
-  if (offset < 70) {
-    phone.style.top = offset / 3 + 250 + "px";
+  if (offset < 250) {
+    phone.style.top = offset / 2 + 350 + "px";
   } else {
-    phone.style.top = 400 + "px";
+    phone.style.top = 500 + "px";
   }
+  // if (pos < 232) {
+  //   phone.style.display = "none";
+  //   phone.style.zIndex = "-1";
+  // }
+
+  console.log(pos);
   // phone.style.display = "none";
 });
 // const bro = (document.getElementById(
