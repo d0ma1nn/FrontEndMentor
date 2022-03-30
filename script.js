35c87b645ba90db09e478d8af3b83cac62148c6e
d0ma1nn <dominikculjak@yahoo.com>
2022-03-30 11:17:41 +0200

    Vitas

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 // x = 23;
 // console.log(x);
 
+
+
 // window.addEventListener("scroll", function () {
 //   let vrednost = window.scrollY;
 //   pho.style.top = vrednost * 0.85 + "px";
@@ -15,37 +17,52 @@
 //   const phone = document.getElementById("content__left");
 //   phone.style.backgroundPositionY = offset * 1 + "px";
 // });
-window.addEventListener("scroll", function () {
-  let offset = window.pageYOffset;
-  console.log(offset);
-  const phone = document.getElementById("absolute");
-  const pos = phone.style.display;
-  // phone.style.transform = translate(40 + "rem" * offset, offset * 100 + "px");
-  // phone.style.top = offset * 0.2 + "px";
-
-  if (offset < 550 && offset > 10) {
-    phone.style.top = offset / 5 + 350 + "px";
-  } else if (offset < 10) {
-    phone.style.top = 600 + "px";
-  } else {
-    phone.style.top = 500 + "px";
-  }
 
 
-  ///// Solution bad 1
-  // if (offset < 450) {
-  //   phone.style.top = Math.abs(offset / 10 + 150 + "px");
-  // } else {
-  //   phone.style.top = 500 + "px";
-  // }
-  // if (pos < 232) {
-  //   phone.style.display = "none";
-  //   phone.style.zIndex = "-1";
-  // }
 
-  console.log(pos);
-  // phone.style.display = "none";
-});
+///////// Bad second one
+// window.addEventListener("scroll", function () {
+//   let offset = window.pageYOffset;
+//   console.log(offset);
+//   const phone = document.getElementById("absolute");
+//   const pos = phone.style.display;
+//   // phone.style.transform = translate(40 + "rem" * offset, offset * 100 + "px");
+//   // phone.style.top = offset * 0.2 + "px";
+
+//   if (offset < 550 && offset > 10) {
+//     phone.style.top = offset / 5 + 350 + "px";
+//   } else if (offset < 10) {
+//     phone.style.top = 600 + "px";
+//   } else {
+//     phone.style.top = 500 + "px";
+//   }
+// });
+
+
+
+
+
+
+
+///// Solution bad 1
+// if (offset < 450) {
+//   phone.style.top = Math.abs(offset / 10 + 150 + "px");
+// } else {
+//   phone.style.top = 500 + "px";
+// }
+// if (pos < 232) {
+//   phone.style.display = "none";
+//   phone.style.zIndex = "-1";
+// }
+
+
+
+
+
+
+// console.log(pos);
+// phone.style.display = "none";
+
 // const bro = (document.getElementById(
 //   "absolute"
 // ).style.transform = `translate(40 + "rem", 100 + "px")`);
